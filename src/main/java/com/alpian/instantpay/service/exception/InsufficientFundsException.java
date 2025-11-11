7929e5c24874d458a175d9a7d8e397fd80ac5985
Lilla Diamantisz <lilladiamantisz@gmail.com>
2025-11-11 19:12:39 +0100

    feat(service): implement InsufficientFundsException

---
+++ src/main/java/com/alpian/instantpay/service/exception/InsufficientFundsException.java
@@ -0,0 +1,13 @@
+package com.alpian.instantpay.service.exception;
+
+public class InsufficientFundsException extends RuntimeException {
+
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+
+    public InsufficientFundsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
