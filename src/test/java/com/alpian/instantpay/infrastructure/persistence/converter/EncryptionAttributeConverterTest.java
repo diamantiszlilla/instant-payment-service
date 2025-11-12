fe1962ef74ff0562e31835a7c14e88e119579130
Lilla Diamantisz <lilladiamantisz@gmail.com>
2025-11-12 16:49:54 +0100

    chore: move directories

--- src/test/java/com/alpian/instantpay/infrastructure/persistence/converter/EncryptionAttributeConverterTest.java
+++ src/test/java/com/alpian/instantpay/infrastructure/persistence/converter/EncryptionAttributeConverterTest.java
@@ -1,6 +1,5 @@
-package com.alpian.instantpay.infrastructure.converter;
+package com.alpian.instantpay.infrastructure.persistence.converter;
 
-import com.alpian.instantpay.infrastructure.persistence.converter.EncryptionAttributeConverter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
